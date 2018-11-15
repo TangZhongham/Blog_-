e68a9d5e2eb3c5c9eb15e8861a80dfca29464c91
Gary Tang <13122260573@163.com>
2018-11-15 15:10:03 +0800

    博客文章正常显示

--- blogproject/blog/views.py
+++ blogproject/blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,get_object_or_404
 from .models import Post
 import markdown
 
+
 def index(request):
     post_list = Post.objects.all().order_by('-created_time')[:3]
     return render(request, 'blog/index.html', context={'post_list': post_list})
--- blogproject/blogproject/settings.py
+++ blogproject/blogproject/settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
+    # 第三方应用
+    'bootstrap3',
 ]
 
 MIDDLEWARE = [
@@ -117,3 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# django-bootstrap3 设置
+BOOTSTRAP3 = {
+    'include_jquery': True,
+}
